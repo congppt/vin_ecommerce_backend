6c9aa05a0ac8e85f70a587cec1c5172607e37fcc
Pham Phuc Thanh Cong (K16 HCM) <congpptse160595@fpt.edu.vn>
2023-06-03 08:00:53 +0700

    update base

---
+++ VinEcomUtility/UtilityMethod/EnumUtility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VinEcomDomain.Enum;
+
+namespace VinEcomUtility.UtilityMethod
+{
+    public static class EnumUtility
+    {
+        public static string GetDisplayName(this ProductCategory category)
+        {
+            return category switch
+            {
+                ProductCategory.Food => "",
+                ProductCategory.Drink => "",
+                ProductCategory.Necessity => "",
+                _ => "",
+            };
+        }
+        public static string GetDisplayName(this OrderStatus status)
+        {
+            return status switch
+            {
+                OrderStatus.Cart => "",
+                OrderStatus.CartCancel => "",
+                OrderStatus.Preparing => "",
+                OrderStatus.Cancel => "",
+                OrderStatus.Done => "",
+                OrderStatus.Shipping => "",
+                _ => "",
+            };
+        }
+        public static string GetDisplayName(this StoreCategory category)
+        {
+            return category switch
+            {
+                StoreCategory.Food => "",
+                StoreCategory.Grocery => "",
+                _ => "",
+            };
+        }
+        public static string GetDisplayName(this VehicleType type)
+        {
+            return type switch
+            {
+                VehicleType.Motorbike => "",
+                _ => "",
+            };
+        }
+    }
+}
--- VinEcomViewModel/Customer/CustomerSignUpViewModel.cs
+++ VinEcomViewModel/Customer/CustomerSignUpViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VinEcomViewModel.Base;
 
 namespace VinEcomViewModel.Customer
 {
-    internal class CustomerSignUpViewModel
+    public class CustomerSignUpViewModel : SignUpViewModel
     {
+        public int BuildingId { get; set; }
     }
 }
